31f6417e82a023d40bb2ffda7de1a2b04a8f20d8
Tyler274 <tyler274port@gmail.com>
2020-11-14 01:29:06 -0500

    working out calibration minutiae.

--- scripts/mendimeter.py
+++ scripts/mendimeter.py
@@ -29,74 +29,87 @@ def mendimeter():
         # Capture frame-by-frame
         ret, frame = cap.read()
         (cols, rows, channels) = frame.shape
+        # cv.flip(frame, 1, frame)
         f_frame = np.float32(frame)
         # upload image to the GPU.
         f_frame = cv.UMat(f_frame)
+        cv.flip(f_frame, 1, f_frame)
 
         # Our operations on the frame come here
         nrows = cv.getOptimalDFTSize(rows)
         ncols = cv.getOptimalDFTSize(cols)
         nimg = cv.resize(f_frame, (nrows, ncols))
 
-        b, g, r = cv.split(nimg)
+        gray = cv.cvtColor(nimg, cv.COLOR_RGBA2GRAY, 0)
 
-        r_dft = cv.dft(
-            r, flags=cv.DFT_COMPLEX_OUTPUT
-        )  # this way the result may fit in the source matrix
-        g_dft = cv.dft(
-            g, flags=cv.DFT_COMPLEX_OUTPUT
-        )  # also doing these inplace for performance.
-        b_dft = cv.dft(b, flags=cv.DFT_COMPLEX_OUTPUT)
+        # b, g, r = cv.split(nimg)
+
+        gray_dft = cv.dft(gray, flags=cv.DFT_COMPLEX_OUTPUT)
+
+        # r_dft = cv.dft(
+        #     r, flags=cv.DFT_COMPLEX_OUTPUT
+        # )  # this way the result may fit in the source matrix
+        # g_dft = cv.dft(
+        #     g, flags=cv.DFT_COMPLEX_OUTPUT
+        # )  # also doing these inplace for performance.
+        # b_dft = cv.dft(b, flags=cv.DFT_COMPLEX_OUTPUT)
 
         # Extract magnitude and phase angle.
-        magR, angleR = cv.cartToPolar(r_dft.get()[:, :, 0], r_dft.get()[:, :, 1])
-        magG, angleG = cv.cartToPolar(g_dft.get()[:, :, 0], g_dft.get()[:, :, 1])
-        magB, angleB = cv.cartToPolar(b_dft.get()[:, :, 0], b_dft.get()[:, :, 1])
+        mag_gray, angle_gray = cv.cartToPolar(
+            gray_dft.get()[:, :, 0], gray_dft.get()[:, :, 1]
+        )
+        # magR, angleR = cv.cartToPolar(r_dft.get()[:, :, 0], r_dft.get()[:, :, 1])
+        # magG, angleG = cv.cartToPolar(g_dft.get()[:, :, 0], g_dft.get()[:, :, 1])
+        # magB, angleB = cv.cartToPolar(b_dft.get()[:, :, 0], b_dft.get()[:, :, 1])
 
-        magR = cv.UMat(magR)
-        magG = cv.UMat(magG)
-        magB = cv.UMat(magB)
+        mag_gray = cv.UMat(mag_gray)
+        # magR = cv.UMat(magR)
+        # magG = cv.UMat(magG)
+        # magB = cv.UMat(magB)
 
         #  switch to logarithmic scale
-        cv.log(magR, magR)
-        cv.log(magG, magG)
-        cv.log(magB, magB)
+        cv.log(mag_gray, mag_gray)
+        # cv.log(magR, magR)
+        # cv.log(magG, magG)
+        # cv.log(magB, magB)
 
-        cv.divide(magR, 30.0, magR)
-        cv.multiply(magG, 1.0 / 30.0, magG)
-        cv.multiply(magB, 1.0 / 30.0, magB)
+        cv.divide(mag_gray, 30.0, mag_gray)
+        # cv.divide(magR, 30.0, magR)
+        # cv.multiply(magG, 1.0 / 30.0, magG)
+        # cv.multiply(magB, 1.0 / 30.0, magB)
 
-        magR = cv.UMat(crop_spectrum(magR.get()))
-        magG = cv.UMat(crop_spectrum(magG.get()))
-        magB = cv.UMat(crop_spectrum(magB.get()))
+        mag_gray = cv.UMat(crop_spectrum(mag_gray.get()))
+        # magR = cv.UMat(crop_spectrum(magR.get()))
+        # magG = cv.UMat(crop_spectrum(magG.get()))
+        # magB = cv.UMat(crop_spectrum(magB.get()))
 
-        # cv.normalize(magR, magR, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        # cv.normalize(magG, magG, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        # cv.normalize(magB, magB, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        gray = cv.cvtColor(nimg, cv.COLOR_RGBA2GRAY, 0)
         gray_8u = cv.normalize(gray, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
         sobelx8u = cv.Sobel(gray_8u, cv.CV_8U, 1, 0, ksize=5)
         sobelx64f = cv.Sobel(gray_8u, cv.CV_64F, 1, 0, ksize=5)
         sobel_8u = cv.convertScaleAbs(sobelx64f)
-        edges = cv.Canny(gray_8u, 50, 150)
+        edges = cv.Canny(gray_8u, 75, 150)
 
         cv.imshow("Canny edges", edges)
         cv.imshow("sobel of grayscale", sobel_8u)
         cv.imshow("worse sobel of grayscale", sobelx8u)
         # Display the resulting frame
-        cv.imshow("cropped for dft", frame)
         cv.imshow(
-            "red channel", cv.normalize(r, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+            "cropped for dft",
+            cv.normalize(f_frame, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U),
         )
-        cv.imshow(
-            "green channel", cv.normalize(g, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        )
-        cv.imshow(
-            "blue channel", cv.normalize(b, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-        )
-        cv.imshow("red channel dft magnitude spectrum", magR)
-        cv.imshow("green channel dft magnitude spectrum", magG)
-        cv.imshow("blue channel dft magnitude spectrum", magB)
+        # cv.imshow(
+        #     "red channel", cv.normalize(r, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+        # )
+        # cv.imshow(
+        #     "green channel", cv.normalize(g, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+        # )
+        # cv.imshow(
+        #     "blue channel", cv.normalize(b, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+        # )
+        cv.imshow("dft magnitude spectrum", mag_gray)
+        # cv.imshow("red channel dft magnitude spectrum", magR)
+        # cv.imshow("green channel dft magnitude spectrum", magG)
+        # cv.imshow("blue channel dft magnitude spectrum", magB)
 
         if cv.waitKey(1) & 0xFF == ord("q"):
             break
